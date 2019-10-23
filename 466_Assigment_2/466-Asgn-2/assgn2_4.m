5d3fe36438967eb350a3d4a535795860b8f693af
kaan taha köken <e215206@metu.edu.tr>
2019-10-24 02:23:14 +0300

    Commends are added

--- 466_Assigment_2/466-Asgn-2/assgn2_4.m
+++ 466_Assigment_2/466-Asgn-2/assgn2_4.m
@@ -2,21 +2,21 @@ clc;
 clear all;
 
 img = im2double(imread("Q4_Input", "tif"));
+%Create filter in order to apply
 filter = [0 -1 0; -1 4 -1; 0 -1 0];
 [M, N] = size(img);
 
 newImg = zeros(M,N);
-
+%apply the filter to the pixels
 for i = 2: M - 1
     for j = 2: N - 1
         newImg(i, j) =  sum(sum(img(i - 1: i + 1, j - 1: j + 1) .* filter));
     end
 end
+% combine original and filtered image
 newImg2 = img + newImg;
 figure;
-subplot(1,3,1);
-imshow(img, []);
-subplot(1,3,2);
+subplot(1,2,1);
 imshow(newImg, []);
-subplot(1,3,3);
+subplot(1,2,2);
 imshow(newImg2, []);
--- 466_Assigment_2/466-Asgn-2/assgn2_5.m
+++ 466_Assigment_2/466-Asgn-2/assgn2_5.m
@@ -2,16 +2,20 @@ clc;
 clear all;
 
 img = im2double(imread("Q5_Input", "jpg"));
+%Converting RGB image to gray scale
 img = .299*img(:,:,1) + .587*img(:,:,2) + .114*img(:,:,3);
+%Create to filter in order to get
+%horizontal and vertical edges
+%As well as we did this on 4th question
 filter = [-1 -2 -1; 0 0 0; 1 2 1];
 reverseFilter = [-1 0 1; -2 0 2; -1 0 1];
 [M, N] = size(img);
 
-
 horz = zeros(M,N);
 vert = zeros(M,N);
 
-
+% Getting only the edges when we applied
+% the each filter
 
 for i = 2: M - 1
     for j = 2: N - 1
@@ -20,18 +24,12 @@ for i = 2: M - 1
     end
 end
 
-%horz = conv2(img, filter, 'same');
-%vert = conv2(img, reverseFilter, 'same');
-
+%Combining the vertical & horizontal images as 1
 edgedImg = sqrt(horz.^2 + vert.^2);
 figure;
-subplot(1,4,1);
+subplot(1,2,1);
 imshow(img, []);
-%subplot(1,4,2);
-%imshow(horz, []);
-%subplot(1,4,3);
-%imshow(vert);
-subplot(1,4,4);
+subplot(1,2,2);
 imshow(edgedImg);
 
 
