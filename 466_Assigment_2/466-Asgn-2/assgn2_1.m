398786f0591fe965f92f62487844a461a9084bd8
kaan taha köken <e215206@metu.edu.tr>
2019-10-24 01:26:52 +0300

    Commends are added

--- 466_Assigment_2/466-Asgn-2/assgn2_1.m
+++ 466_Assigment_2/466-Asgn-2/assgn2_1.m
@@ -5,29 +5,30 @@ input_1 = imread("Q1_Input_1.tif");
 input_2 = imread("Q1_Input_2.tif");
 
 figure;
-subplot(3,2,1);
-imshow(input_1);
-subplot(3,2,2);
-imhist(input_1, 64);
-mod_img_1 = histeq(input_1);
-subplot(3,2,3);
-imshow(mod_img_1);
-subplot(3,2,4);
-imhist(mod_img_1, 64);
+%subplot(1,2,1);
+%imshow(input_2);
 
-%s = whos("Q1_Input_1.tif");
 %gray level range 0 to 255
-[M, N] = size(input_2);
-gray_level = grayLevel(M, N, input_2);
+[M, N] = size(input_1);
+gray_level = grayLevel(M, N, input_1);
+%Normalized the gray level by dividing area of the image
 normalized_level = gray_level/(M * N);
 
 transformation = trans(256, normalized_level);
 transformation = round(transformation);
+% After we get the integer values of each bit, we are
+% again dividing transformed image to area of the image. 
 histEq = transformation / (M * N);
 
-final = assgnValue(histEq, M, N, input_2);
-subplot(3,2,5);
+%Lastly, We are assigning values to new Image.
+final = assgnValue(histEq, M, N, input_1);
+%subplot(1,2,2);
 imshow(final, []);
+
+%{
+    With this function, I am finding the occurence of the grays in the
+    image. For example, 0 (index 1 at the matrix) can be occured 100 times.
+%}
 
 function levels = grayLevel(dimX, dimY, image)
     level = zeros(1, 256);
@@ -39,6 +40,15 @@ function levels = grayLevel(dimX, dimY, image)
     end
     levels = level;
 end
+
+%{
+    I transformed the normalized gray levels with this function. Size is
+    the related with image. We are looking that how many bits the image contains. For example If image 8
+    bit then size 0 to 255 or 1 to 256
+    
+    In each iteration, we are summing up the past values to new value, and
+    we are multiplying with the size - 1 (because bits between 0 to 255)
+%}
 
 function tranf = trans(size, norm_level)
     arr = zeros(1, size);
